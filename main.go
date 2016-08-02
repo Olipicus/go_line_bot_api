ad4666e47417cd26b591f0fbbe27c442d05c6613
TheerapatH <thetlpc@hotmail.com>
2016-08-02 16:24:30 +0700

    Print Request Body

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", lineBotHandler)
+	http.ListenAndServe(":9900", nil)
+}
+
+func lineBotHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("%s", body)
+}
