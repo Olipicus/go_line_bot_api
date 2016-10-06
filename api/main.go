c750b45ec63360ef7a70cc442acb1223fcedefd4
TheerapatH <thetlpc@hotmail.com>
2016-10-06 12:16:09 +0700

    Use linebot API

--- api/main.go
+++ api/main.go
@@ -1,22 +1,86 @@
 package main
 
 import (
-	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-func main() {
-	http.HandleFunc("/line", lineBotHandler)
-	http.ListenAndServe(":9900", nil)
+//LineApp :
+type LineApp struct {
+	bot *linebot.Client
 }
 
-func lineBotHandler(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
-
-	err := ioutil.WriteFile("output.txt", body, 0644)
+//NewLineApp : New LineApp
+func NewLineApp(channelSecret, channelToken string) (*LineApp, error) {
+	bot, err := linebot.New(
+		channelSecret,
+		channelToken,
+	)
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	return &LineApp{
+		bot: bot,
+	}, nil
+}
+
+func main() {
+	app, err := NewLineApp(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	w.Write([]byte(body))
+	http.HandleFunc("/linebot", app.callbackHandler)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func (app *LineApp) callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := app.bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	log.Printf("Got events %v", events)
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch event.Message.(type) {
+			case *linebot.TextMessage:
+				if event.Source.UserID != "" {
+					profile, err := app.bot.GetProfile(event.Source.UserID).Do()
+					if err != nil {
+						app.replyText(event.ReplyToken, err.Error())
+					}
+					if _, err := app.bot.ReplyMessage(
+						event.ReplyToken,
+						linebot.NewTextMessage("Display name: "+profile.DisplayName),
+						linebot.NewTextMessage("Status message: "+profile.StatusMessage),
+					).Do(); err != nil {
+						log.Fatal(err)
+					}
+				} else {
+					app.replyText(event.ReplyToken, "Bot can't use profile API without user ID")
+				}
+			}
+		}
+	}
+}
+
+func (app *LineApp) replyText(replyToken, text string) error {
+	if _, err := app.bot.ReplyMessage(
+		replyToken,
+		linebot.NewTextMessage(text),
+	).Do(); err != nil {
+		return err
+	}
+	return nil
 }
